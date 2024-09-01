8e1cd254a84e4c2b8f0c560ea5d1d091f0ff5736
Rahi Bulbul <rahibulbulrahi@gmail.com>
2024-09-01 15:00:18 +0100

    Base
    ready

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar/page";
+
+export default function Home() {
+  return (
+    <div className="screen">
+      <Navbar />
+      <div className="web-container">Home</div>
+    </div>
+  );
+}
